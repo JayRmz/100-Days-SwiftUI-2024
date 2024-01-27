49a7f22812e6541afa2739b93674ffdd35013321
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-01-27 02:08:33 -0600

    Day3

--- Day 3.playground/Contents.swift
+++ Day 3.playground/Contents.swift
@@ -2,4 +2,74 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-// --MARK:
+// --MARK: How to store ordered data in arrays
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Allen")
+beatles.append("Adrian")
+beatles.append("Novall")
+beatles.append("Vivian")
+
+//temperatures.append("Chris")
+
+let firstBeatle = beatles[0]
+let firstNumber = numbers[0]
+//let notAllowed = firstBeatle + firstNumber
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+print(scores[1])
+
+//var albums = Array<String>()
+//albums.append("Folklore")
+//albums.append("Fearless")
+//albums.append("Red")
+
+var albums = [String]()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let reversedPresidents = presidents.reversed()
+print(reversedPresidents)
+
+// --MARK: How to store and find data in dictionaries
+let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+print(employee2["name"])
+print(employee2["job"])
+print(employee2["location"])
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Batman"] = "Penguin"
+
+// --MARK: How to use sets for fast data lookup
+//let people = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+//print(people)
+var people = Set<String>()
+people.insert("Denzel Washington")
+people.insert("Tom Cruise")
+people.insert("Nicolas Cage")
+people.insert("Samuel L Jackson")
+
+// --MARK: How to create and use enums
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+var day = Weekday.monday
+day = Weekday.tuesday
+day = Weekday.friday
