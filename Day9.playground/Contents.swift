d1957fdce49e6282eb0865ae2eacb6cc1017035b
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-02-25 17:54:38 -0600

    Day 10

--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -2,27 +2,16 @@ import UIKit
 
 //MARK: Day 9: Closures
 
-//How to create and use closures
-func greetUser() {
-    print("Hi there!")
-}
-
-greetUser()
-
-var greetCopy = greetUser
-greetCopy()
-
+// How to create and use closures
 //let sayHello = {
 //    print("Hi there!")
 //}
-//
+
 //sayHello()
 
 let sayHello = { (name: String) -> String in
     "Hi \(name)!"
 }
-
-sayHello("Jay")
 
 func getUserData(for id: Int) -> String {
     if id == 1989 {
@@ -35,3 +24,93 @@ func getUserData(for id: Int) -> String {
 let data: (Int) -> String = getUserData
 let user = data(1989)
 print(user)
+
+sayHello("Taylor")
+
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+func captainFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+}
+//let captainFirstTeam = team.sorted(by: captainFirstSorted)
+
+// How to use trailing closures and shorthand syntax
+//
+//let captainFirstTeam = team.sorted { name1, name2 in
+//    if name1 == "Suzanne" {
+//        return true
+//    } else if name2 == "Suzanne" {
+//        return false
+//    }
+//
+//    return name1 < name2
+//}
+
+let captainFirstTeam = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+
+    return $0 < $1
+}
+
+print(captainFirstTeam)
+
+//Reverse sorted
+let reverseTeam = team.sorted { $0 > $1 }
+print(reverseTeam)
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
+
+
+
+// How to accept functions as parameters
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+
+    return numbers
+}
+
+let rolls = makeArray(size: 50) {
+    Int.random(in: 1...20)
+}
+
+print(rolls)
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third: {
+    print("This is the third work")
+}
