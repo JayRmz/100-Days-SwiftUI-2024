00a41753bdbf524b967ca7ace34a0379580187e5
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-02-26 14:14:19 -0600

    Day 11

--- Day10.playground/Contents.swift
+++ Day10.playground/Contents.swift
@@ -111,3 +111,96 @@ struct Player {
 
 let player = Player(name: "Megan R")
 print(player.number)
+
+
+// MARK: Structs pt. 2
+
+// How to limit access to internal data using access control
+struct BankAccount {
+    private var funds = 0
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool{
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+
+//account.funds -= 1000 //This is not allowed since funds is private to the structure
+
+
+// Static properties and methods
+struct School {
+    static var studentCount = 0
+    static func add(student: String) {
+        print("\(student) joined the school")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Jay Rmz")
+print(School.studentCount)
+
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+
+struct EmployeeTwo {
+    let username: String
+    let password: String
+
+    static let example = EmployeeTwo(username: "cfederighi", password: "hairforceone")
+}
+
+
+// MARK: Checkpoint 6
+struct Car {
+    let model: String     // constant! probably won't change
+    let seatCount: Int    // same!
+    private(set) var currentGear = 1  // only change via an internal method.
+
+    enum GearDirection {
+        case up, down, neutral
+    }
+
+    public mutating func changeGear(_ direction: GearDirection) {
+        switch direction {
+        case .down : currentGear -= 1
+            if currentGear < 1 {currentGear = 1}
+        case .up :   currentGear += 1
+            if currentGear > 10 { currentGear = 10 }
+        case .neutral:
+            currentGear = 1  // arbitrary selection
+        }
+        print("The \(model) is in gear: \(currentGear)")
+    }
+}
+
+// Test Code
+var prefect = Car(model: "Ford Prefect", seatCount: 4, currentGear: 3)
+prefect.changeGear(.up)
+prefect.changeGear(.neutral) // Jump right to Gear 1.
+prefect.changeGear(.down)  // try this line a couple of times. try to get below zero.
+
+
+
